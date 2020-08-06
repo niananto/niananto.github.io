858f0be954a174f84c01b203f31545009f30681e
Nazmul Islam Ananto <nazmulislamananto@gmail.com>
2020-08-07 05:18:46 +0600

    changed icon and transition in dark mode icon

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -9,6 +9,7 @@ function darkMode() {
         var divWindowLeft = document.querySelector("div#window-left");
         var divWindowRight = document.querySelector("div#window-right");
         var divWindowRightLinks = document.querySelectorAll("div#window-right a:link, div#window-right a:visited");
+        var darkModeIcon = document.querySelector("a.dark-mode-icon i");
 
         if ( divWindowLeft.style.borderRightStyle === "unset" ) {
             divWindowLeft.style.borderRightStyle = "outset";
@@ -21,6 +22,8 @@ function darkMode() {
                 divWindowRightLinks[i].style.color = "#d3fcff";
             }
 
+            darkModeIcon.setAttribute("class", "fas fa-toggle-on");
+
         } else {
             divWindowLeft.style.borderRightStyle = "unset";
             divWindowLeft.style.borderRightStyle = "unset";
@@ -31,6 +34,8 @@ function darkMode() {
             for ( let i=0; i < divWindowRightLinks.length; i++ ){
                 divWindowRightLinks[i].style.color = "#006cb0";
             }
+
+            darkModeIcon.setAttribute("class", "fas fa-toggle-off");
 
         }
 

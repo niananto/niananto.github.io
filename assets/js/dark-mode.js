1333b97e3b2cb122c4cc48accd23a6aa04a97c19
Nazmul Islam Ananto <nazmulislamananto@gmail.com>
2020-08-08 17:12:01 +0600

    fixed dark mode color in projects

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -9,11 +9,15 @@ function darkMode() {
         var divWindow = document.querySelector(".window");
         var divWindowLeft = document.querySelector("div#window-left");
         var divWindowRight = document.querySelector("div#window-right");
+        // var divWindowRights = document.querySelectorAll("div#window-right *");
         var divWindowRightLinks = document.querySelectorAll("div#window-right a:link, div#window-right a:visited");
         var darkModeIcon = document.querySelector("a.dark-mode-icon i");
         var contactWrapper = document.querySelector("#window-right > main > div > article > div > div.wrapper");
         var contactUs = document.querySelector("div.contact-us");
         var contactUsInput = document.querySelectorAll("div.contact-us input");
+        var project = document.getElementsByClassName("project");
+        var readmeContainer = document.querySelector('#readme-container');
+        var githubChart = document.querySelector("#github-chart");
 
         if ( divWindowLeft.style.borderRightStyle == "unset" ) {
 
@@ -25,6 +29,11 @@ function darkMode() {
             // important na dileo hocche right now
             divWindowRight.style.setProperty("background", "#121212", "important");
             divWindowRight.style.setProperty("color", "white", "important");
+
+            // for ( let i=0; i<divWindowRights.length; i++ ){
+            //     divWindowRights[i].style.setProperty("background", "#121212", "important");
+            //     divWindowRights[i].style.setProperty("color", "white", "important");
+            // }
         
             for ( let i=0; i < divWindowRightLinks.length; i++ ){
                 divWindowRightLinks[i].style.color = "#d3fcff";
@@ -32,13 +41,22 @@ function darkMode() {
 
             darkModeIcon.setAttribute("class", "fas fa-toggle-on");
 
-            contactWrapper.style.setProperty('background', 'url("/assets/img/contact-bg-4.jpg") center center no-repeat');
+            if(contactWrapper){
+                contactWrapper.style.setProperty('background', 'url("/assets/img/contact-bg-4.jpg") center center no-repeat');
+                contactUs.style.setProperty('background', '#ddd');
+            }
 
-            contactUs.style.setProperty('background', '#ddd');
-
-            for( let i=0; i < contactUsInput.length; i++ ){
+            for ( let i=0; i < contactUsInput.length; i++ ){
                 contactUsInput[i].style.setProperty("background", "#ddd");
             }
+
+            for ( let i=0; i < project.length; i++ ){
+                project[i].style.setProperty("background", "#ddd");
+            }
+
+            readmeContainer.style.setProperty('background', '#ccc');
+
+            githubChart.style.setProperty('background', '#ddd');
 
         } else {
 
@@ -50,19 +68,33 @@ function darkMode() {
             divWindowRight.style.setProperty("background", "#f2faff");
             divWindowRight.style.setProperty("color", "black");
 
+            // for ( let i=0; i<divWindowRights.length; i++ ){
+            //     divWindowRights[i].style.setProperty("background", "#f2faff");
+            //     divWindowRights[i].style.setProperty("color", "black");
+            // }
+
             for ( let i=0; i < divWindowRightLinks.length; i++ ){
                 divWindowRightLinks[i].style.color = "#006cb0";
             }
 
             darkModeIcon.setAttribute("class", "fas fa-toggle-off");
 
-            contactWrapper.style.setProperty('background', 'url("/assets/img/contact-bg-2.jpg") center center no-repeat');
-
-            contactUs.style.setProperty('background', '#f2faff');
+            if(contactWrapper){
+                contactWrapper.style.setProperty('background', 'url("/assets/img/contact-bg-2.jpg") center center no-repeat');
+                contactUs.style.setProperty('background', '#f2faff');
+            }
 
             for( let i=0; i < contactUsInput.length; i++ ){
                 contactUsInput[i].style.setProperty("background", "#f2faff");
             }
+
+            for ( let i=0; i < project.length; i++ ){
+                project[i].style.setProperty("background", "rgb(232,244,248)");
+            }
+
+            readmeContainer.style.setProperty('background', 'linear-gradient(to right, rgba(232,244,248,1), white)');
+
+            githubChart.style.setProperty('background', 'linear-gradient(to right, rgba(232,244,248,1), white)');
 
         }
 
